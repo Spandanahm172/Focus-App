4c7a84afe0157d39af2a8266c2fb0323c09244f2
Your Name <you@example.com>
2023-05-14 14:18:05 +0530

    Code Changement

--- Script.js
+++ Script.js
@@ -1,14 +1,24 @@
-const taskinput = document.getElementById('input');
-const tasklist = document.getElementById('li');
-function addtask(){
-    const task= taskinput.value;
-    if(task){
-        const list= document.createElement('list');
-        list.innerHTML= `
-        <span> ${task} </span>
-        `;
-        tasklist.appendChild(list);
-        taskinput.value="";
+// Get necessary elements from the DOM
+const taskInput = document.getElementById('task-input');
+const taskList = document.getElementById('task-list');
 
-    }
+// Function to add a new task
+function addTask() {
+  const task = taskInput.value;
+  if (task) {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <input type="checkbox">
+      <span>${task}</span>
+      <button onclick="removeTask(this)">Delete</button>
+    `;
+    taskList.appendChild(li);
+    taskInput.value = '';
+  }
+}
+
+// Function to remove a task
+function removeTask(button) {
+  const li = button.parentElement;
+  taskList.removeChild(li);
 }
